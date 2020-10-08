d2aa34dd4fcdd7cc18f207228296c9979c6959f2
zasimpson99 <72581190+zasimpson99@users.noreply.github.com>
2020-10-08 16:08:30 -0600

    Update and rename motorstepresponsevariables.m to motor step response variables.m

--- motor step response variables.m
+++ motor step response variables.m
@@ -1,13 +1,13 @@
+%%Determined by matching experimental to simulation
 sigma = 11.45;
 k = 1.62;
-i = 16;
-w =16;
+%%Experimnetal data array
 time = .001*[1000.00 1050.00 1100.00 1150.00 1202.00 1252.00 1302.00 1352.00 1403.00 1454.00 1504.00 1554.00 1605.00 1655.00 1705.00 1756.00 1807.00 1857.00 1907.00 1957.00]; 
 velocity = [1.57 3.65 4.75 5.34 5.65 5.81 5.89 5.93 5.93 5.97 5.93 5.97 5.97 5.93 5.97 5.97 5.93 5.97 5.97 5.97];
-
+%%Data from simulation
 out=sim('motorstepresponse');
 
-
+%%Graphing data
 figure(1)
 plot(out.simout,'-','color','blue');
 hold on
